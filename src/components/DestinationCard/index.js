6ecec6d4e555adfc7cbca09caa13a3e6e65e4db6
llja <lesterlim.ja@gmail.com>
2023-09-23 11:39:58 +0800

    plan for surprise page

---
+++ src/components/DestinationCard/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import axios from 'axios';
+
+const DestinationCard=()=>{
+
+  return(
+    // there is 2 variation of this, one with search inputs and 1 with database inputs
+    // create a generic one that can support both and build a gallery view of all available flights
+    console.log('this is a card that displays a picture of the destination country, details of the flight, price of the flight, dates travelling if applicable')
+  )
+}
+
+export default DestinationCard;
