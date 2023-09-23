3f633dd058d1ddfa306e569cd18876140c90506e
zach7815 <53745970+zach7815@users.noreply.github.com>
2023-09-23 15:42:06 +0800

    fix: fixed select options error that closed dropdown when clicking option

--- src/components/ModelContainer.js
+++ src/components/ModelContainer.js
@@ -1,18 +1,37 @@
-import React, { forwardRef } from 'react';
+// ModelContainer.js
+import React, { forwardRef, useState } from 'react';
 import withClickOutside from './withClickOutside';
+import NestedForm from './DropDown';
 
 const ModelContainer = forwardRef(({ open, setOpen }, ref) => {
+  const [adultCounterValue, setAdultCounterValue] = useState(0);
+  const [childCounterValue, setChildCounterValue] = useState(0);
+
   return (
     <section ref={ref}>
-      <h1> demonstration </h1>
-      <button onClick={() => setOpen(!open)}>dropdown toggle</button>
-
-      {open && (
-        <ul>
-          <li>one</li>
-          <li>two</li>
-        </ul>
-      )}
+      <div className="toggle-menu">
+        <button
+          onClick={(event) => {
+            event.preventDefault();
+            setOpen(!open);
+          }}
+        >
+          <span className="placeholder-text"> Travellers and cabin Class</span>
+          <span className="option text">
+            {adultCounterValue} Adult, First Class
+          </span>
+        </button>
+        {open && (
+          <NestedForm
+            className="passenger-type-accordian"
+            // Pass the handler function
+            adultCounterValue={adultCounterValue}
+            setAdultCounterValue={setAdultCounterValue}
+            childCounterValue={childCounterValue}
+            setChildCounterValue={setChildCounterValue}
+          />
+        )}
+      </div>
     </section>
   );
 });
